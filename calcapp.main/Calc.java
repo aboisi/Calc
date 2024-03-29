45f9d79db2377cc876d4f74eca09994fb79edab8
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-29 15:58:13 +0900

    修正

--- calcapp.main/Calc.java
+++ calcapp.main/Calc.java
@@ -4,7 +4,7 @@ public class Calc {
     public static void main(String[] args) {
         int a = 10; int b = 2;
         int total = CalcLogic.tasu(a, b);//FQCNでなくてもエラーにならない
-        int delta = CalcLogic.hiku(a, b);//FQCNを指定してもよい
+        int delta = calcapp.logics.CalcLogic.hiku(a, b);//FQCNを指定してもよい
         System.out.println("足すと" + total + "、引くと" + delta);
     }
     
